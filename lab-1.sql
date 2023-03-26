9165b184424a7126f05ec3a45eff2ce2daffbf24
SashaChekh004 <113788577+SashaChekh004@users.noreply.github.com>
2023-03-26 13:16:20 +0300

    Update lab-1.sql

--- lab-1.sql
+++ lab-1.sql
@@ -1,5 +1,5 @@
-SELECT * from cities ORDER BY population DESC LIMIT 10 OFFSET 10
-SELECT * from cities ORDER BY name DESC LIMIT 30
-SELECT * from cities ORDER BY region, population DESCSELECT * from cities ORDER BY region, population DESC
-SELECT DISTINCT region from cities
-SELECT name, region FROM cities ORDER BY region DESC, name DESC
+SELECT * FROM cities ORDER BY population DESC LIMIT 10 OFFSET 10;
+SELECT * FROM cities ORDER BY name DESC LIMIT 30;
+SELECT * FROM cities ORDER BY region, population DESCSELECT * from cities ORDER BY region, population DESC;
+SELECT DISTINCT region from cities;
+SELECT name, region FROM cities ORDER BY region DESC, name DESC;
